538746e3f2caaaef1d12b4612abe8198c6603250
victor <vdiazroa@gmail.com>
2024-06-02 20:05:22 +0200

    move usestate to component

---
+++ app/helpers/index.tsx
@@ -0,0 +1,58 @@
+import { FC, useEffect, useState } from "react"
+import Image from "next/image";
+import Link from "next/link";
+
+const Main: FC = () => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [reading, setReading] = useState<boolean>(false)
+    const [cards, serCards] = useState<string[]>([])
+
+
+    const Card: FC<{ card: string }> = ({ card }) => {
+        return <Image
+            src={`https://optcgplayer.com/images/EN/${card}.png`}
+            alt={card}
+            // className="dark:invert"
+            width={30}
+        />
+    }
+
+    const getCards = async () => {
+        console.log("#### fetching cards")
+        const res = await fetch("/api/cards")
+        console.log("####",res)
+        const { cards, reading } = (await res.json())
+        serCards(cards)
+        setLoading(false)
+        setReading(reading)
+
+        if (reading) {
+            await new Promise((res) => { setTimeout(() => res(true), 500) })
+            getCards()
+        }
+    }
+
+
+
+    useEffect(() => {
+        getCards()
+    }, [])
+
+    return <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <Link href="/api/cards">
+        click </Link>
+        {cards.map(card => <Card card={card} />)}
+        {!loading ? "loading...." : <button
+            onClick={async () => {
+                await fetch(reading ? "/api/cards/stop" : "/api/cards/start?cards=5")
+                setReading(!reading)
+                if (reading) { getCards() }
+            }
+            } >
+            {reading ? "Stop" : "Start"}
+        </button>}
+    </div>
+
+}
+
+export default Main
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,10 @@
-import { FC, useEffect, useState } from "react"
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import Main from "./helpers";
 
 export default function Home(): JSX.Element {
-  const [loading, setLoading] = useState<boolean>(true)
-  const [reading, setReading] = useState<boolean>(false)
-  const [cards, serCards] = useState<string[]>([])
-
-
-  const Card: FC<{ card: string }> = ({ card }) => {
-    return <Image
-      src={`https://optcgplayer.com/images/EN/${card}.png`}
-      alt={card}
-      // className="dark:invert"
-      width={30}
-    />
-  }
-
-  const getCards = async () => {
-    console.log("#### fetching cards")
-    const res = await fetch("/api/cards")
-    const { cards, reading } = (await res.json())
-    serCards(cards)
-    setLoading(false)
-    setReading(reading)
-
-    if (reading) {
-      await new Promise((res) => { setTimeout(() => res(true), 500) })
-      getCards()
-    }
-  }
-
-
-
-  useEffect(() => {
-    getCards()
-  }, [])
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {cards.map(card => <Card card={card} />)}
-        {!loading ? "loading...." : <button
-          onClick={async () => {
-            await fetch(reading ? "/api/cards/stop" : "/api/cards/start?cards=5")
-            setReading(!reading)
-            if (reading) { getCards() }
-          }
-          } >
-          {reading ? "Stop" : "Start"}
-        </button>}
-      </div>
+      <Main />
     </main>
   );
 }
